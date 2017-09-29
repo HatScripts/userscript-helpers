e4fc5e8dd0a5451dec597826fd3150ada1fff354
Nick Holmes <contact@hatscripts.com>
2017-09-29 13:26:33 +1000

    Create ready.js

---
+++ ready.js
@@ -0,0 +1,15 @@
+/**
+ * Specify a function to execute when the DOM is fully loaded.
+ * @param {function} fn - A function to execute after the DOM is ready.
+ * @see https://learn.jquery.com/using-jquery-core/document-ready/
+ * @see http://youmightnotneedjquery.com/#ready
+ * @see https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+ * @see https://caniuse.com/#feat=domcontentloaded
+ */
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
