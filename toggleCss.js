36405814a4cd949e607e4759ab94c81c22008deb
Nick Holmes <contact@hatscripts.com>
2017-12-29 17:17:36 +1100

    Merge hashCode fuction into toggleCss function
    Since it doesn't/shouldn't need to be called from outside the toggleCss function. Also removing the hashCode function helps with minifying to a smaller size.

--- toggleCss.js
+++ toggleCss.js
@@ -1,22 +1,14 @@
 /**
- * Hashes a string.
- * @param {String} str - The string to hash.
- * @returns {number} The generated hash code.
- * @see https://stackoverflow.com/a/34842797/2203482
- */
-function hashCode(str) {
-    return str.split('').reduce((prevHash, currVal) =>
-        ((prevHash << 5) - prevHash) + currVal.charCodeAt(0), 0);
-}
-
-/**
  * Toggles a string of CSS within the document.
  * If the specified CSS has not been applied to the page, it will be, else it will be removed.
  * @param {String} css - A string of CSS.
  * @returns {boolean} Whether the CSS was enabled or disabled.
  */
 function toggleCss(css) {
-    var id = '' + hashCode(css);
+    // Get hash of the CSS string to use as the id
+    var id = '' + css.split('').reduce(function (prevHash, currVal) {
+        return ((prevHash << 5) - prevHash) + currVal.charCodeAt(0)
+    }, 0);
     var style = document.getElementById(id);
     if (style) {
         style.parentNode.removeChild(style);
