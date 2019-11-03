a48bd262311608e089569f2f00f78812ca6ff29c
HatScripts <contact@hatscripts.com>
2019-11-03 14:05:32 +1100

    Add option to minify the CSS
    * https://gist.github.com/orangexception/1292778
    * https://regex101.com/r/jKbuGY
    
    For example:
    ```css
    div {
    /* Test 1 */
    /* Test * / * 2 */
    /* Test
    . test
    . test
    
    test 3
    */
    /* Test * / * 4 */ width:	0; /* Test 5 *//**/
    /* Test 6 /*/
    }
    ```
    becomes:
    ```css
    div{width:0;}
    ```

--- applyCss.js
+++ applyCss.js
@@ -1,9 +1,13 @@
 /**
  * Applies a string of CSS to the document. It creates a new <style>
  * element, adds the given CSS to it, and inserts it into the <head>.
- * @param {String} css - A string of CSS.
+ * @param {string} css - A string of CSS.
+ * @param {boolean} minify - Whether to apply simple regex-based minification to the CSS string.
  */
-function applyCss (css) {
+function applyCss (css, minify = true) {
+  if (minify) {
+    css = css.replace(/\s|\/\*.*?\*\//gs, '')
+  }
   const style = document.createElement('style')
   style.appendChild(document.createTextNode(css))
   const parent = document.head || document.body || document.documentElement
