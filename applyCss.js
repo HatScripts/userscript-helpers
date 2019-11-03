63b74e5c6226e7f1114809e686cb256636245a4b
HatScripts <contact@hatscripts.com>
2019-11-03 14:15:54 +1100

    Remove option to minify the CSS
    * because it's unreliable, e.g. it will remove all whitespace, including within strings
    * because it's redundant; adding a style element to the DOM has very little overhead, whether the CSS is minified or not

--- applyCss.js
+++ applyCss.js
@@ -2,12 +2,8 @@
  * Applies a string of CSS to the document. It creates a new <style>
  * element, adds the given CSS to it, and inserts it into the <head>.
  * @param {string} css - A string of CSS.
- * @param {boolean} minify - Whether to apply simple regex-based minification to the CSS string.
  */
-function applyCss (css, minify = true) {
-  if (minify) {
-    css = css.replace(/\s|\/\*.*?\*\//gs, '')
-  }
+function applyCss (css) {
   const style = document.createElement('style')
   style.appendChild(document.createTextNode(css))
   const parent = document.head || document.body || document.documentElement
